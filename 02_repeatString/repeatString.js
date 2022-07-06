efa83993c07f77c357839db7951b772d14dfb0dd
alexthwining <14134483+alexthwining@users.noreply.github.com>
2022-07-06 15:54:44 -0700

    completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+	let complete = "";
+	if(n < 0) {
+		//throw new RangeError("Argument must be greater than 0");
+		return "ERROR";
+	}
+	for(let i=0; i < n; i++) {
+		complete = complete.concat(str);
+	}
 
+	return complete;
 };
 
 // Do not edit below this line
