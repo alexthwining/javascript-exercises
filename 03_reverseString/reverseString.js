64eed40a6f11bfe9554bde8445d3a39f6739b767
alexthwining <14134483+alexthwining@users.noreply.github.com>
2022-07-06 18:59:18 -0700

    working on exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,22 @@
-const reverseString = function() {
+const reverseString = function(str) {
+	let words = str.split(' ');
+	console.log(words.length);
+	let rev = "";
+	if(words.length > 1) {
+	    for(let i=0; i < words.length; i++) {
+	        let wrd = words[i].split('');
+		
+	    }
+
+	}
+	else {
+	    let wrd = words[0].split('');
+	    for(let i=0; i < wrd.length; ) {
+	    rev = rev + wrd.pop();
+	}
+
+
+	return rev;
 
 };
 
