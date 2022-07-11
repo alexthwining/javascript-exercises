b2da9e3c3fe9a0970cfa9cf89f55dc9edf0e2d4e
alexthwining <14134483+alexthwining@users.noreply.github.com>
2022-07-10 19:05:23 -0700

    completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    /*
+        arguments: a, b -> both integers
 
+        let sum = 0;
+        if a > b
+            swap if true
+        end
+
+        loop: a to b (inclusive), starting at a
+            sum += a;
+        end
+
+        return sum
+    */
+
+    if(a < 0 || b < 0 || typeof a != "number" || typeof b != "number" ) {
+        return "ERROR";
+    }
+    
+    if(a > b) {
+        let t = b;
+        b = a;
+        a = t;
+    }
+
+    let sum = 0;
+    for(; a <= b; a++) {
+        sum += a;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
