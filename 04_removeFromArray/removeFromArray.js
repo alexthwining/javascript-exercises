e57f26016c86254c792dcbc04212cb7e3ab4e69c
alexthwining <14134483+alexthwining@users.noreply.github.com>
2022-07-10 18:53:11 -0700

    copmleted exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -5,52 +5,27 @@ const removeFromArray = function(arr, ...args) {
 
         res = [] -> result array
 
-        get first argument: args.shift()
-        let lookFor = args.shift()
-
-        loop: iterate i
-            find index of lookFor
-            if i == lookFor; continue;
-
-            res.push(arr[i])
-
-            if: (args.length > 0)
-                lookFor = args.shift()
+        loop: iterate through arr
+            check if element included in arr
+            if not in arr,
+                res.push(arr[i])
             end
         end
 
     */
     
-    console.log(arr);
-
     let res = [];
-    let lookFor = args.shift(); // first argument
-    console.log("starting lookFor:" + lookFor);
-    let i = 0;
-    for( ; i < arr.length; i++) {
-        let ind = arr.indexOf(lookFor);
-        console.log("index of LF: " + ind);
-        console.log("i :" + i);
-        if (ind == i) {
-            console.log("skipped: " + arr[i]);
-            continue; 
+    arr.forEach((ele) => {
+        // found out about Array.includes() which makes it a lot easier
+        if(!args.includes(ele)) {
+            res.push(ele);
         }
+    });
 
-        res.push(arr[i]);
-        
-        if(arr.indexOf(lookFor) && args.length > 0) {
-            i = 0;
-            lookFor = args.shift();
-            console.log("lookFor inside: " + lookFor);
-        }
-    }
-
-    console.log(res);
     return res;
+
 };
 
-// testing
-console.log(removeFromArray([1,2,3,4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
